464f18308b1db5409a332e9ef2d112129f288763
Marco Lehmann <m99@posteo.de>
2021-01-08 11:09:39 +0100

    Update webhook

--- api/cf-webhook.js
+++ api/cf-webhook.js
@@ -2,8 +2,20 @@
 // A Node.js Serverless Function as described here:
 // - https://vercel.com/docs/serverless-functions/introduction
 // - https://vercel.com/guides/handling-node-request-body
-module.exports = async (req, res) => {
-  const { body } = req
-  console.log(body)
-  res.send(`Hello ${body.sys.id}, you just parsed the request body`)
+// - https://nodejs.org/ja/docs/guides/anatomy-of-an-http-transaction/#request-body
+module.exports = (req, res) => {
+  console.log(req)
+
+  let body = []
+  req.on('data', (chunk) => {
+    body.push(chunk)
+  }).on('end', () => {
+    body = Buffer.concat(body).toString()
+    // at this point, `body` has the entire request body stored in it as a string
+
+    console.log(body)
+
+    // ping pong
+    res.json(req)
+  })
 }
