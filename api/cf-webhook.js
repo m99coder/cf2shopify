7f0ccee516f5a5f7d44d98ecfddd4404a235e0e0
Marco Lehmann <m99@posteo.de>
2021-01-08 12:23:35 +0100

    Update webhook

--- api/cf-webhook.js
+++ api/cf-webhook.js
@@ -3,18 +3,11 @@
 // - https://vercel.com/docs/serverless-functions/introduction
 // - https://vercel.com/guides/handling-node-request-body
 // - https://nodejs.org/ja/docs/guides/anatomy-of-an-http-transaction/#request-body
+
+// Be careful to define `application/json` as the content type of the triggering webhook,
+// otherwise the following code will not work
 module.exports = (req, res) => {
-  console.log(req)
-
-  let body = ''
-  req.on('data', (chunk) => {
-    body += chunk.toString('utf8')
-  })
-
-  req.on('end', () => {
-    console.log(body)
-    let json = JSON.parse(body)
-    console.log(json)
-    res.json(json)
-  })
+  const { body } = req
+  console.log(body.fields)
+  res.json(body)
 }
